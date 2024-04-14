4dbc50be0791e4658ee0b7d269ae51bd470a2b2b
ayeshashanzay <164702342+ayeshashanzay@users.noreply.github.com>
2024-04-14 14:42:59 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+import inquirer from "inquirer";
+const currency = {
+    USD: 1,
+    EUR: 0.97,
+    GBP: 0.76,
+    INR: 74.57,
+    PRK: 280,
+};
+let user_answer = await inquirer.prompt([
+    {
+        name: "from",
+        message: "Enter your currency",
+        type: "list",
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PRK']
+    },
+    {
+        name: "to",
+        message: "Enter to currency",
+        type: "list",
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PRK']
+    },
+    {
+        name: "amount",
+        message: "Enter your amount",
+        type: "number"
+    }
+]);
+let fromCurrency = currency[user_answer.from];
+let toCurrency = currency[user_answer.to];
+let amount1 = user_answer.amount;
+let baseCurrency = amount1 / fromCurrency;
+let currencyConverter = baseCurrency * toCurrency;
+console.log(currencyConverter);
